a3339a9f115ad021789f53b5d48ca1e433082cd7
jeunlee <junniesu@gmail.com>
2016-12-01 18:35:34 -0500

    just started thurs

--- browser/react/components/Album.js
+++ browser/react/components/Album.js
@@ -1,14 +1,55 @@
 import React from 'react';
 import Songs from '../components/Songs';
 
-const Album = (props) => {
+export default class extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      album: {}
+    }
+  }
+
+  componentDidMount(){
+    axios.get(`/api/albums/${albumId}`)
+      .then(res => res.data)
+      .then(album => this.setState({
+        album:
+      }))
+
+
+
+  }
+
+  render() {
+    return (
+          // {console.log(props)}
+    <div className="album">
+      <div>
+        <h3>{ album.name }</h3>
+        <img src={ album.imageUrl } className="img-thumbnail" />
+      </div>
+      <Songs
+        songs={album.songs}
+        currentSong={currentSong}
+        isPlaying={isPlaying}
+        toggleOne={toggleOne} />
+    </div>
+    );
+  }
+
+}
+/*
+const Album = (props) => {*/
 
   const album = props.album;
   const currentSong = props.currentSong;
   const isPlaying = props.isPlaying;
   const toggleOne = props.toggleOne;
 
+/*  console.log(props);
+
   return (
+          // {console.log(props)}
     <div className="album">
       <div>
         <h3>{ album.name }</h3>
@@ -24,3 +65,4 @@ const Album = (props) => {
 }
 
 export default Album;
+*/
